a16ec445e259a51c6423d2706900ac3b8768dc30
Christopher Dajer <chrisdajer@gmail.com>
2021-01-28 01:33:12 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,18 +7,22 @@ def subtraction(num1, num2)
 num1 - num2
 end
 subtraction(2,4)
+
 def division(num1, num2)
 num1 / num2
 end
 division(2,4)
+
 def multiplication(num1, num2)
 num1 * num2
 end
 multiplication(2,4)
+
 def modulo(num1, num2)
 num1 % num2
 end
 modulo(2, 4)
+
 def square_root(num)
 
 end
