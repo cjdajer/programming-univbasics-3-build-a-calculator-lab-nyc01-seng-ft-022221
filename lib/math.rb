2cde76f52f006b6904b7ddcbf7e97f080cfba937
Christopher Dajer <chrisdajer@gmail.com>
2021-01-28 01:29:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,17 +4,17 @@ end
 addition(2, 4)
 
 def subtraction(num1, num2)
-
+num1 - num2
 end
-
+subtraction(2,4)
 def division(num1, num2)
-
+num1 / num2
 end
-
+division(2,4)
 def multiplication(num1, num2)
-
+num1 * num2
 end
-
+multiplication(2,4)
 def modulo(num1, num2)
 
 end
